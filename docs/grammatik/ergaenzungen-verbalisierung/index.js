24b89ea9e46a8f12ebeb541cababa7065b9f2e0d
Andrés Bonilla <gabonillaa2004@gmail.com>
2026-07-23 23:08:14 +0200

    Auto-commit: 2026-07-23 23:08:14

--- docs/grammatik/ergaenzungen-verbalisierung/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.body.classList.remove("no-js");
-
-  requestAnimationFrame(() => {
-    document.body.classList.add("is-ready");
-  });
-});
--- docs/grammatik/passiv/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.body.classList.remove("no-js");
-  
-  requestAnimationFrame(() => {
-    document.body.classList.add("is-ready");
-  });
-});
-
